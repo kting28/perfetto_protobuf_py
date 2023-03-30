fefbab18fcbde8ca73a2537138ab013f4be3a510
Kai Ting <kkting@gmail.com>
2023-03-30 11:59:50 -0700

    Use Trace message to contain the packets

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Introduction
-These are perfetto protobuf definitions compiled for python. [Source](https://cs.android.com/android/platform/superproject/+/master:external/perfetto/protos/perfetto/) can be found here. Not fully working yet since the output file is not recognized by Perfetto UI. See the [synthetic track event guide](https://perfetto.dev/docs/reference/synthetic-track-event) for some documentation on creating these events.
+These are perfetto protobuf definitions compiled for python. [Source](https://cs.android.com/android/platform/superproject/+/master:external/perfetto/protos/perfetto/) can be found here. 
+
+See the [synthetic track event guide](https://perfetto.dev/docs/reference/synthetic-track-event) for some documentation on creating these events.
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from protos.perfetto.trace import trace_packet_pb2 as trace_packet
+from protos.perfetto.trace import trace_pb2 as trace
 from protos.perfetto.config import trace_config_pb2 as trace_config
 from protos.perfetto.trace.track_event import track_event_pb2 as track_event
 from protos.perfetto.trace.track_event import track_descriptor_pb2 as track_descriptor
@@ -9,21 +10,19 @@ TYPE_SLICE_END = track_event.TrackEvent.Type.TYPE_SLICE_END
 TYPE_INSTANT = track_event.TrackEvent.Type.TYPE_INSTANT
 TYPE_COUNTER = track_event.TrackEvent.Type.TYPE_COUNTER
 
-packet_list = []
+trace = trace.Trace()
 
-tc = trace_config.TraceConfig()
-ds = trace_config.TraceConfig.DataSource() 
-ds.config.name = "track_event"
-tc.data_sources.append(ds)
-
-packet_list.append(tc)
-
+#tc = trace_config.TraceConfig()
+#ds = trace_config.TraceConfig.DataSource() 
+#ds.config.name = "track_event"
+#tc.data_sources.append(ds)
+#
+#
 tp = trace_packet.TracePacket()
 tp.track_descriptor.uuid = 894893984 
 tp.track_descriptor.process.pid = 1234
 tp.track_descriptor.process.process_name = "My process name"
-
-packet_list.append(tp)
+trace.packet.append(tp)
 
 
 tp = trace_packet.TracePacket()
@@ -33,7 +32,7 @@ tp.track_descriptor.thread.pid = 1234
 tp.track_descriptor.thread.tid = 5678
 tp.track_descriptor.thread.thread_name = "My thread name"
 
-packet_list.append(tp)
+trace.packet.append(tp)
 
 tp = trace_packet.TracePacket()
 
@@ -43,7 +42,7 @@ tp.track_event.track_uuid = 49083589894
 tp.track_event.name = "My special parent"
 tp.trusted_packet_sequence_id = 3903809
 
-packet_list.append(tp)
+trace.packet.append(tp)
 
 tp = trace_packet.TracePacket()
 tp.timestamp = 250
@@ -52,7 +51,7 @@ tp.track_event.track_uuid = 49083589894
 tp.track_event.name = "My special child"
 tp.trusted_packet_sequence_id = 3903809
 
-packet_list.append(tp)
+trace.packet.append(tp)
 
 tp = trace_packet.TracePacket()
 
@@ -61,7 +60,7 @@ tp.track_event.type = TYPE_SLICE_END
 tp.track_event.track_uuid = 49083589894
 tp.trusted_packet_sequence_id = 3903809
 
-packet_list.append(tp)
+trace.packet.append(tp)
 
 tp = trace_packet.TracePacket()
 tp.timestamp = 300
@@ -69,10 +68,8 @@ tp.track_event.type = TYPE_SLICE_END
 tp.track_event.track_uuid = 49083589894
 tp.trusted_packet_sequence_id = 3903809
 
-packet_list.append(tp)
 
-with open("./output.bin", "wb") as fd:
-    for p in packet_list:
-        print(p)
-        fd.write(p.SerializeToString())
+with open("./output.perfetto-trace", "wb") as fd:
+    print(trace)
+    fd.write(trace.SerializeToString())
 
