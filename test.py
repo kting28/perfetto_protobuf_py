e3ece28e14b56c8a30f50627e76044685ba885fd
Kai Ting <kkting@gmail.com>
2023-04-08 20:23:26 -0700

    Add "process-scoped" async slices example

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from os import write
 import gen.perfetto_trace_pb2 as trace
 
 # Generate a 64-bit uuid
@@ -132,6 +133,110 @@ tp.track_event.track_uuid = my_thread_uuid
 tp.trusted_packet_sequence_id = trusted_packet_sequence_id
 top.packet.append(tp)
 
+
+#--------------------------------------------------------------------------------
+#                       Process-scoped (sync) slices
+#--------------------------------------------------------------------------------
+#  in the legacy JSON tracing format, this section corresponds to b/e/n events
+#  with the associated M (metadata) events. source:
+#  https://perfetto.dev/docs/reference/synthetic-track-event
+
+#  Process-scoped slices are useful to trace execution of a "piece of work"
+#  across multiple threads of a process. A process-scoped slice can start on a
+#  thread A and end on a thread B. Examples include work submitted to thread
+#  pools and coroutines.
+
+# Process tracks can be named corresponding to the executor and can also have
+# child slices in an identical way to thread-scoped slices. Importantly, this
+# means slices on a single track must strictly nest inside each other without
+# overlapping.
+# As separating each track in the UI can cause a lot of clutter, the UI visually
+# merges process tracks with the same name in each process. Note that this does
+# not change the data model (e.g. in trace processor tracks remain separated) as
+# this is simply a visual grouping.
+#--------------------------------------------------------------------------------
+# The first track associated with this process
+tp = trace.TracePacket()
+process2_uuid = uuid64()
+tp.track_descriptor.uuid = process2_uuid
+tp.track_descriptor.name = "My special track"
+tp.track_descriptor.process.pid = 4321
+tp.track_descriptor.process.process_name = "My process2 name"
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 200
+tp.track_event.type = TYPE_SLICE_BEGIN
+tp.track_event.track_uuid = process2_uuid
+tp.track_event.name = "My special parent A"
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 250
+tp.track_event.type = TYPE_SLICE_BEGIN
+tp.track_event.track_uuid = process2_uuid
+tp.track_event.name = "My special child"
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 290
+tp.track_event.type = TYPE_SLICE_END
+tp.track_event.track_uuid = process2_uuid
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 300
+tp.track_event.type = TYPE_SLICE_END
+tp.track_event.track_uuid = process2_uuid
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
+#--------------------------------------------------------------------------------
+# The second track associated with this process. Note how we make the above
+# track the "parent" of this track: this means that this track also is
+# associated to the same process. Note further this shows as the same visual
+# track in the UI but remains separate in the trace and data model. Emitting
+# these events on a separate track is necessary because these events overlap
+# *without* nesting with the above events.
+#--------------------------------------------------------------------------------
+second_track_uuid = uuid64()
+tp = trace.TracePacket()
+tp.track_descriptor.uuid = second_track_uuid
+tp.track_descriptor.name = "My special track"
+tp.track_descriptor.parent_uuid = process2_uuid #above track is made parent
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 230
+tp.track_event.type = TYPE_SLICE_BEGIN
+tp.track_event.track_uuid = second_track_uuid
+tp.track_event.name = "My special parent A"
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 260
+tp.track_event.type = TYPE_SLICE_BEGIN
+tp.track_event.track_uuid = second_track_uuid
+tp.track_event.name = "My special child"
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 270
+tp.track_event.type = TYPE_SLICE_END
+tp.track_event.track_uuid = second_track_uuid
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
+
+tp = trace.TracePacket()
+tp.timestamp = 295
+tp.track_event.type = TYPE_SLICE_END
+tp.track_event.track_uuid = second_track_uuid
+tp.trusted_packet_sequence_id = trusted_packet_sequence_id
+top.packet.append(tp)
 
 # Write the data out as binary
 with open("./output.perfetto-trace", "wb") as fd:
